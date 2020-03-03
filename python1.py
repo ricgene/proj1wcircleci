63e791b64cf97087117ec4e4df73ddf21722cabe
Richard Genet <richard.genet@gmail.com>
2020-03-02 20:03:56 -0500

    add python

---
+++ python1.py
@@ -0,0 +1,7 @@
+print("in file python1")
+
+# comment
+
+def hi():
+      #x = 1
+    return('hi')
---
+++ test_python1.py
@@ -0,0 +1,9 @@
+from python1 import *
+
+
+#d = "."
+#print 'This is test code',d
+def test_hi():
+    rslt = hi()
+    print('This is the rslt',rslt)
+    assert rslt == 'hi'
